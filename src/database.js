375ae4eb98f334f0d62a14d6efafe623e2cb0b51
Minn <pc2.myn@gmail.com>
2020-05-06 12:02:44 +0700

    b

--- src/database.js
+++ src/database.js
@@ -1,15 +1,15 @@
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 const mongoDB = 'mongodb+srv://shaymin:9899@cluster0-hgfdj.mongodb.net/test?retryWrites=true&w=majority'
 
 
 
 
 function getConnect(){
-    mongooes.connect(mongoDB || process.env.MONGODB_URL,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true });
+    mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true });
     
-    mongooes.Promise = global.Promise;
+    mongoose.Promise = global.Promise;
 
-    const db = mongooes.connection;
+    const db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     db.once('open', () => console.log('Database connected'))
